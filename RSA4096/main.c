e266f859d0bd1821d3fc87533a838560505e3bc3
hjh <1561868283@qq.com>
2021-10-18 13:36:27 +0800

    add print_array

--- RSA4096/main.c
+++ RSA4096/main.c
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-10-12 18:47:44
- * @LastEditTime: 2021-10-18 09:49:24
+ * @LastEditTime: 2021-10-18 13:35:58
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \RSA\RSA4096\RSA_4096_origin_private\main.c
@@ -18,36 +18,47 @@ Description : Rsa4096
 #include <stdlib.h>
 #include "rsa.h"
 #include "keys.h"
+void print_array(char *TAG, uint8_t *array, int len)
+{
+	int i;
+
+	printf("%s[%d]: ", TAG, len);
+	for(i=0; i<len; i++) {
+		printf("%02X", array[i]);
+	}
+	printf("\n");
+}
+
 const int count=10;
 int private_enc_dec_test()
 {
 	uint8_t input[512*2]={0};
-    rsa_pk_t pk = {0};
-    rsa_sk_t sk = {0};
-    uint8_t  output[512*2]={0};
+	rsa_pk_t pk = {0};
+	rsa_sk_t sk = {0};
+	uint8_t  output[512*2]={0};
 	unsigned char msg [512*2]={0};
-    uint32_t msg_len;
-    uint32_t outputLen;
-    int32_t inputLen;
+	uint32_t msg_len;
+	uint32_t outputLen;
+	int32_t inputLen;
 
-    printf("RSA encryption decryption test is beginning!\n");
-    printf("\n");
-    pk.bits = KEY_M_BITS;
-    memcpy(&pk.modulus         [RSA_MAX_MODULUS_LEN-sizeof(key_m)],  key_m,  sizeof(key_m));
-    memcpy(&pk.exponent        [RSA_MAX_MODULUS_LEN-sizeof(key_e)],  key_e,  sizeof(key_e));
-    sk.bits = KEY_M_BITS;
-    memcpy(&sk.modulus         [RSA_MAX_MODULUS_LEN-sizeof(key_m)],  key_m,  sizeof(key_m));
-    memcpy(&sk.public_exponet  [RSA_MAX_MODULUS_LEN-sizeof(key_e)],  key_e,  sizeof(key_e));
-    memcpy(&sk.exponent        [RSA_MAX_MODULUS_LEN-sizeof(key_pe)], key_pe, sizeof(key_pe));
-    memcpy(&sk.prime1          [RSA_MAX_PRIME_LEN-sizeof(key_p1)],   key_p1, sizeof(key_p1));
-    memcpy(&sk.prime2          [RSA_MAX_PRIME_LEN-sizeof(key_p2)],   key_p2, sizeof(key_p2));
-    memcpy(&sk.prime_exponent1 [RSA_MAX_PRIME_LEN-sizeof(key_e1)],   key_e1, sizeof(key_e1));
-    memcpy(&sk.prime_exponent2 [RSA_MAX_PRIME_LEN-sizeof(key_e2)],   key_e2, sizeof(key_e2));
-    memcpy(&sk.coefficient     [RSA_MAX_PRIME_LEN-sizeof(key_c)],    key_c,  sizeof(key_c));
+	printf("RSA encryption decryption test is beginning!\n");
+	printf("\n");
+	pk.bits = KEY_M_BITS;
+	memcpy(&pk.modulus         [RSA_MAX_MODULUS_LEN-sizeof(key_m)],  key_m,  sizeof(key_m));
+	memcpy(&pk.exponent        [RSA_MAX_MODULUS_LEN-sizeof(key_e)],  key_e,  sizeof(key_e));
+	sk.bits = KEY_M_BITS;
+	memcpy(&sk.modulus         [RSA_MAX_MODULUS_LEN-sizeof(key_m)],  key_m,  sizeof(key_m));
+	memcpy(&sk.public_exponet  [RSA_MAX_MODULUS_LEN-sizeof(key_e)],  key_e,  sizeof(key_e));
+	memcpy(&sk.exponent        [RSA_MAX_MODULUS_LEN-sizeof(key_pe)], key_pe, sizeof(key_pe));
+	memcpy(&sk.prime1          [RSA_MAX_PRIME_LEN-sizeof(key_p1)],   key_p1, sizeof(key_p1));
+	memcpy(&sk.prime2          [RSA_MAX_PRIME_LEN-sizeof(key_p2)],   key_p2, sizeof(key_p2));
+	memcpy(&sk.prime_exponent1 [RSA_MAX_PRIME_LEN-sizeof(key_e1)],   key_e1, sizeof(key_e1));
+	memcpy(&sk.prime_exponent2 [RSA_MAX_PRIME_LEN-sizeof(key_e2)],   key_e2, sizeof(key_e2));
+	memcpy(&sk.coefficient     [RSA_MAX_PRIME_LEN-sizeof(key_c)],    key_c,  sizeof(key_c));
 
 	generate_rand(input,1000);
 	inputLen = strlen((const char*)input);
-    // private key encrypt
+	// private key encrypt
 	clock_t start,end;
 	double sum=0,sum1=0;
 	int status=0;
@@ -73,7 +84,7 @@ int private_enc_dec_test()
 	}
 	printf("rsa_public_encrypt_any_len Average time(s): %lf; rsa_private_decrypt_any_len Average time(s): %lf\n",sum/count,sum1/count);
 	print_array("input ",input,inputLen);
-    print_array("rsa_public_encrypt_any_len", output, outputLen);
+	print_array("rsa_public_encrypt_any_len", output, outputLen);
 	print_array("rsa_public_decrypt_any_len", msg, msg_len);
 	if(memcmp(input,msg,inputLen)!=0){
 		printf("rsa_public_encrypt_any_len and rsa_private_decrypt_any_len Error\n");
@@ -82,10 +93,10 @@ int private_enc_dec_test()
 	else{
 		printf("Public Encrypt and private decrypt success!\n");
 	}
-    return 0;
+	return 0;
 }
 int main(int argc, char const *argv[])
 {
-    private_enc_dec_test();
+	private_enc_dec_test();
 	// public_enc_dec();
 }
